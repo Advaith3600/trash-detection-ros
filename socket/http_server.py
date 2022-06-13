adde0f6b66b324bcc0e9c38809cc48e3c5405b45
Darkdriller <swaindhruv28@gmail.com>
2022-06-14 03:09:59 +0530

    added direction

--- socket/http_server.py
+++ socket/http_server.py
@@ -1,3 +1,4 @@
+#Imports
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import socket
@@ -11,73 +12,87 @@ from pathlib import Path
 import math
 import json
 import torch
+import base64
+#load Model from localhost Custom
 model = torch.hub.load('ultralytics/yolov5', 'custom', 'D:\\OneDrive\\Desktop\\ros\\yolov5-master\\runs\\train\\exp4\\weights\\best.torchscript',force_reload=True)
-def get_dist(p1,p2):
-    return math.sqrt((p1[1]-p2[1])**2 + (p1[0]-p2[0])**2)
-def get_ang(xmin,ymin,xmax,ymax,img):
-    mp_of_img= [(xmax-xmin)//2,(ymax-ymin)//2]
-    mp_of_frame= [(img.shape[1])//2,0]
-    hyp=get_dist(mp_of_img,mp_of_frame)
-    base=(ymax-ymin)//2
-    cos_theta=base/hyp
-    return(math.acos(cos_theta))
+
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-
-    def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
-
     def do_POST(self):
         global model
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        # logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-        #         str(self.path), str(self.headers), post_data.decode('utf-8'))
-        frame=None
-        try:
-            frame = pickle.loads(post_data)
-        except:
-            if frame:
-                df_result = results.pandas().xyxy[0]
-                dict_result = df_result.to_dict()
-                # print(dict_result)
-                scores = list(dict_result["confidence"].values())
-                labels = list(dict_result["name"].values())
-                if dict_result["confidence"]:
-                    highest_pred_label=max(dict_result["confidence"])
-                    highest_pred_val=dict_result["confidence"][highest_pred_label]
-                    list_boxes = list()
-                    # print(df_result.to_dict('records'))
-                    dict_item=df_result.to_dict('records')[highest_pred_label]
-                    list_boxes.append(list(dict_item.values())[:4])
-                    count = 0
-                    for xmin, ymin, xmax, ymax in list_boxes:
-                        ang=get_ang(xmin,ymin,xmax,ymax,frame)
-                        frame = cv2.rectangle(frame, pt1=(int(xmin),int(ymin)), pt2=(int(xmax),int(ymax)), \
-                                                 color=(255,0, 0), thickness=2)
-                        cv2.putText(frame, f"{labels[count]}: {round(highest_pred_val, 2)}", (int(xmin), int(ymin)-10), \
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-                        self._set_response()
-                        self.wfile.write(str(ang).encode('utf-8'))
-                    cv2.imshow('Object Detector',frame)
-                else:
+        frame = pickle.loads(post_data)
+        print("Does this work?")
+        if len(frame)!=0:
+            results = model(frame)
+            df_result = results.pandas().xyxy[0] # <--- converts result into panda
+            dict_result = df_result.to_dict()
+            # scores = list(dict_result["confidence"].values()) # <--- gets confidence values
+            # labels = list(dict_result["name"].values()) # <--- gets names
+            if dict_result["confidence"]:
+                highest_pred_label=max(dict_result["confidence"])# <--- selects label with highest confidence
+                highest_pred_val=dict_result["confidence"][highest_pred_label]
+                list_boxes = list()
+                dict_item=df_result.to_dict('records')[highest_pred_label]# <---gets record of highest confidence label
+                list_boxes.append(list(dict_item.values())[:4])# <--- Gets the bounding box for records
+                count = 0
+                for xmin, ymin, xmax, ymax in list_boxes:
+                    ang,dir=get_ang(xmin,ymin,xmax,ymax,frame)
+                    frame = cv2.rectangle(frame, pt1=(int(xmin),int(ymin)), pt2=(int(xmax),int(ymax)), \
+                                             color=(255,0, 0), thickness=2) # <--- prints bounding box
+                    cv2.putText(frame, f"{labels[count]}: {round(highest_pred_val, 2)}", (int(xmin), int(ymin)-10), \
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2) # <--- prints confidence values
+                    cv2.imwrite("data.jpg",frame)
                     self._set_response()
-                    self.wfile.write("Could not detect a plastic bottle".encode('utf-8'))
+                    message_dict={"status":1}
+                    message_dict["angle"]=ang
+                    message_dict["dir"]=dir
+                    message = json.dumps(message_dict)
+                    # cv2.imshow('Object Detector',frame)
+                    self.wfile.write(bytes(message,encoding="utf-8"))
 
             else:
-
                 self._set_response()
-                self.wfile.write("Give me a cv2 byte image DAMMMIT".encode('utf-8'))
+                message_dict={"status":0}
+                message_dict["angle"]="Bottle Not FOund"
+                cv2.imwrite("data.jpg",frame)
+                message = json.dumps(message_dict)
+                self.wfile.write(bytes(message,encoding='utf-8'))
+        else:
+            self._set_response()
+            message_dict={"status":2}
+            message_dict["angle"]="No frame"
+            message = json.dumps(message_dict)
+            self.wfile.write(bytes(message,encoding="utf-8"))
+
+
+#get dist bw 2 points
+def get_dist(p1,p2):
+    return math.sqrt((p1[1]-p2[1])**2 + (p1[0]-p2[0])**2)
+#calculate angle between two
+def get_ang(xmin,ymin,xmax,ymax,img):
+    mp_of_img= (int((xmin+xmax)/2),int((ymin+ymax)/2))
+    mp_of_frame= (int((img.shape[1])//2),img.shape[0])
+    print(mp_of_img,xmin,ymin,xmax,ymax)
+    img=cv2.line(img,mp_of_img,mp_of_frame,(0,255,0),2)
+    hyp=get_dist(mp_of_img,mp_of_frame)
+    base=int((ymin+ymax)/2)
+    img=cv2.line(img,((img.shape[1])//2,int(base)),mp_of_frame,(0,255,0),2)
+    cos_theta=base/hyp
+    dir="L"
+    if mp_of_img[0]>mp_of_frame[0]:
+        dir="R"
+
+    return(math.acos(cos_theta)*180/3.14,dir)
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(level=logging.INFO)
-    server_address = ('192.168.1.3', port)
+    server_address = ('192.168.43.154', port)
     httpd = server_class(server_address, handler_class)
     logging.info('Starting httpd...\n')
     try:
